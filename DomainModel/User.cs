9ccbba3d5b34e7ab3b14b86619bb1b86f3e20b78
lkri <lkri@techcollege.dk>
2023-04-12 10:21:51 +0200

    added CompareTo for user

--- DomainModel/User.cs
+++ DomainModel/User.cs
@@ -1,9 +1,16 @@
-﻿namespace DomainModel
+﻿using System.Collections;
+
+namespace DomainModel
 {
-    public class User
+    public class User : IComparable
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public int CompareTo(object? obj)
+        {
+            User user = obj as User;
+            return Id.CompareTo(user.Id);
+        }
     }
 }
